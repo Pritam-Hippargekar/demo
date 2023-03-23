b0120cc5788192ac2a4ba7b9f35873856872fd51
Pritam-Hippargekar <tech.pritam358@gmail.com>
2023-03-23 19:03:51 +0530

    address changes

--- src/main/java/com/elastic/data/domain/Address.java
+++ src/main/java/com/elastic/data/domain/Address.java
@@ -3,29 +3,41 @@ package com.elastic.data.domain;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.*;
+
+import java.util.List;
+
 @Data
 @RequiredArgsConstructor
 public class Address {
     @Field(type = FieldType.Long, name = "id")
     private Long id;
 
-    @Field(type = FieldType.Text, name = "street", analyzer = "ik_max_word")
-    private String street;
+    @Field(type = FieldType.Text, name = "address_line", analyzer = "ik_max_word")
+    private String addressLine;
 
-    @Field(type = FieldType.Integer)
-    private Integer houseNumber;
+    @Field(type = FieldType.Keyword)
+    private String country;
 
     @Field(type = FieldType.Keyword)
     private String state;
 
     @Field(type = FieldType.Keyword)
-    private String city;
-
-    @Field(type = FieldType.Keyword)
     private String district;
 
-    @Field(type = FieldType.Text, name = "zip_code")
-    private String zipCode;
+    @Field(type = FieldType.Keyword)
+    private String townName;
+
+    @Field(type = FieldType.Text, name = "street_name", analyzer = "ik_max_word")
+    private String streetName;
+
+    @Field(type = FieldType.Integer)
+    private Integer buildingNumber;
+
+    @Field(type = FieldType.Integer)
+    private Integer houseNumber;
+
+    @Field(type = FieldType.Text, name = "post_code")
+    private String postCode;
 
     @GeoPointField
     @Field(type = FieldType.Object,name = "location")
