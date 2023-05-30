852e0c840b844056703f2af9e2073a13508468db
Pritam-Hippargekar <tech.pritam358@gmail.com>
2023-05-30 18:04:26 +0530

    done changes

--- src/main/java/com/elastic/data/validation/FileSizeValidator.java
+++ src/main/java/com/elastic/data/validation/FileSizeValidator.java
@@ -24,4 +24,48 @@ public class FileSizeValidator implements ConstraintValidator<FileSize, Multipar
 
         return multipartFile.getSize()<maxSizeInMB*MB;
     }
+
 }
+
+
+
+
+
+//    String strFilePath = "C:/data.sql.gz";
+//    //File object denoting the file
+//    File file = new File(strFilePath);
+//        if(file.exists()){
+//                long lSizeInBytes = file.length();
+//                System.out.println("File size in bytes: " + lSizeInBytes);
+//
+//                //divide bytes by 1024 to get size in kilobytes
+//                double dSizeInKB = lSizeInBytes/1024;
+//                System.out.println("File size in kilobytes (KB): " + dSizeInKB);
+//
+//                //divide kilobytes by 1024 to get size in megabytes
+//                double dSizeInMB = dSizeInKB/1024;
+//                System.out.println("File size in megabytes (MB): " + dSizeInMB);
+//
+//                //divide megabytes by 1024 to get size in gigabytes
+//                double dSizeInGB = dSizeInMB/1024;
+//                System.out.println("File size in gigabytes (GB): " + dSizeInGB);
+//
+//                //divide gigabytes by 1024 to get size in terabytes
+//                double dSizeInTB = dSizeInGB/1024;
+//                System.out.println("File size in terabytes (TB): " + dSizeInTB);
+//
+//          }else{
+//
+//                System.out.println("File does not exists");
+//         }
+
+
+
+
+//    How to get a file extension in Java?
+//    int intLastDotPosition = strFileName.lastIndexOf(".");
+//    int intSlashPosition = strFileName.lastIndexOf( System.getProperty("file.separator") );
+//
+//    if( intLastDotPosition > intLastSlashPosition ){
+//            strFileExtension = strFileName.substring(intLastDotPosition + 1);
+//    }
